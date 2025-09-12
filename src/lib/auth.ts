4cb0327989a2d07241bc8c71373fa779310b7fbd
NathanWebDev <pikminnicus@gmail.com>
2025-09-12 07:34:39 -0500

    Added Google Login Logic

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -13,6 +13,11 @@ export const auth = betterAuth({
       discord: {
         clientId: process.env.DISCORD_CLIENT_ID as string, 
         clientSecret: process.env.DISCORD_CLIENT_SECRET as string, 
+      },
+      google: {
+        prompt: "select_account", 
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
       }
     },
     database: drizzleAdapter(db, {
