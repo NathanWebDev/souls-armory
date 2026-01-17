52a8913407e130cabd73dee63a57ff02fd69110b
NathanWebDev <pikminnicus@gmail.com>
2026-01-17 13:53:10 -0600

    Added placeholder cards to prepare for dynamic data

--- src/app/components/item/page.tsx
+++ src/app/components/item/page.tsx
@@ -3,10 +3,20 @@ import { Card } from "@/components/ui/card";
 export default function ItemTile(){
     return(
         <>
-            <Card className="p-4 m-4 w-150px flex flex-col sm:flex-row items-center">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/d/dd/Square_-_black_simple.svg" alt="Item Image" style={{width: "64px", aspectRatio: "1/1", objectFit: "cover"}}/>
-                <p>This is an item tile component.</p>
-            </Card>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 m-4 mx-15 md:mx-4 lg:mx-4">
+                <Card className="flex flex-col md:flex-row items-center min-h-32 md:min-h-auto">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/d/dd/Square_-_black_simple.svg" alt="Item Image" className="w-16 aspect-square object-cover"/>
+                    <p>This is an item tile component.</p>
+                </Card>
+                <Card className="flex flex-col md:flex-row items-center min-h-32 md:min-h-auto">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/d/dd/Square_-_black_simple.svg" alt="Item Image" className="w-16 aspect-square object-cover"/>
+                    <p>This is an item tile component.</p>
+                </Card>
+                <Card className="flex flex-col md:flex-row items-center min-h-32 md:min-h-auto">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/d/dd/Square_-_black_simple.svg" alt="Item Image" className="w-16 aspect-square object-cover"/>
+                    <p>This is an item tile component.</p>
+                </Card>
+            </div>
         </>
     )
 }
